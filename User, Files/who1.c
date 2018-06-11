a3ebba4900d1f9dc0fdb6d0680003515ffecdd20
ryan <zruan@bu.edu>
2018-06-10 23:56:48 -0400

    who1.c finished

--- User, Files/who1.c
+++ User, Files/who1.c
@@ -22,17 +22,20 @@ show_info(struct utmp *utbufp) {
 }
 
 int main() {
-    struct utmp current_record;
-    int utmpfd;
+    /*
+     * utmp is the data structure stored in umpt.h. It stores info about users logged into the machine.
+     */
+    struct utmp current_record; /* read info into this struct*/
+    int utmpfd; /* file descriptor to read from */
     int reclen = sizeof(current_record);
 
-    if ( (utmpfd = open (UTMP_FILE, O_RDONLY)) == -1) {
+    if ( (utmpfd = open (UTMP_FILE, O_RDONLY)) == -1) { /* if fail to open UTMP_FILE (defined in utmp.h) */
         perror( UTMP_FILE );
         _exit(1);
     }
-    while ( read(utmpfd, &current_record, reclen) == reclen ) {
+    while ( read(utmpfd, &current_record, reclen) == reclen ) { /* read size reclen data from utmpfd to current_record */
         show_info(&current_record);
     }
-    close(utmpfd);
+    close(utmpfd);  /* close file descriptor utmpfd */
     return 0;
 }
