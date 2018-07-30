e371fd1affb39b48acb870d1753f4c6e0d8af3ea
Ryan <zruan@bu.edu>
2018-07-30 13:40:12 -0400

    Shell

---
+++ Shell/exec1.c
@@ -0,0 +1,9 @@
+main() {
+  char *arglist[3];
+  arglist[0] = "ls";
+  arglist[1] = "-l";
+  arglist[2] = 0;
+  printf("* * * About to exec ls -l\n");
+  execvp("ls",arglist);
+  printf("* * * ls is done. bye\n");
+}
